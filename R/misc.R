4008e9f59b093b6807d757942e010aaa898c730f
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2017-11-01 08:07:16 -0500

    add function to retrieve the name of the denominator

--- R/misc.R
+++ R/misc.R
@@ -13,3 +13,21 @@ norm_identity <- function(mat) {
 geomean <- function(x, na.rm=TRUE){
   exp(sum(log(x[x > 0]), na.rm = na.rm) / length(x))
 }
+
+#' Get Denom Name
+#' 
+#' Function to retrieve the denominator for a sleuth object
+#' processed using compositional data analysis.
+#'
+#' @param obj the sleuth object
+#' @return The name of the denominator(s) for the sleuth object
+#' @export
+get_denom_name <- function(obj) {
+  stopifnot(is(obj, "sleuth"))
+  name <- environment(obj$transform_fun)$denom_name
+  if (is.null(name)) {
+    stop("This sleuth object was not processed using compositional data analysis.",
+         "\nThus its denominator does not exist.")
+  }
+  name
+}
